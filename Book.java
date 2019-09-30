07e5f87fe31d478a088a688801519718b7510b59
Chaithrodaya BS <chaith13007@gmail.com>
2019-09-30 12:29:43 -0700

    adding author

--- Book.java
+++ Book.java
@@ -3,4 +3,10 @@ class Book
 	private int bookId;
 	private String name;
 	private int price;
+	private String author;
+	
+	public void setauthor(String authorname)
+	{
+		this.authorname = authorname;
+	}
 }
